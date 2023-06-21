e191667f78d44df6edd9e726a15921ad1c1e44ad
louiml <99025570+Louiml@users.noreply.github.com>
2023-06-21 12:29:52 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Chat from './Chat.svelte';
+  </script>
+  
+  <Chat />
+  
+  <style>
+	:global(body) {
+	  margin: 0;
+	  padding: 0;
+	  font-family: Arial, sans-serif;
+	  background-color: rgb(34, 34, 40);
+	  color: white;
+	}
+  </style>
+  
